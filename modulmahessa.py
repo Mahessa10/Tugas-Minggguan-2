8e896095280697c18d4285c3e9af5a15df4590bc
Mahessa10 <121791163+Mahessa10@users.noreply.github.com>
2023-03-09 06:14:10 +0700

    Add files via upload

---
+++ modulmahessa.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello, " + name) 
+
+person1 = {
+    "name": "Ishfy Achmed Mahessa",
+    "NIM": 312010118,
+    "class": "TI20E1",
+    "age": 21,
+    "country": "Indonesia"
+}
